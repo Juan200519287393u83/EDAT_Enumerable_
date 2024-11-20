cd11d08c05faeda4cd80f6770863eeee4791d97f
Juan200519287393u83 <23030133@itesa.edu.mx>
2024-11-20 08:20:53 -0600

    Versión 02: Lista con foreache, clear, sort orderiby.

--- Enumerable.UI.Consola/Program.cs
+++ Enumerable.UI.Consola/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("-------------------------------");
+        Console.WriteLine(" Enumerables ");
+        Console.WriteLine("-------------------------------");
+
+        #region Lista
+
+        var lista = new List<string>
+        {
+            "Juan",
+            "Fleny",
+            "Jan",
+            "Pania",
+            "Jose",
+            "Alex",
+            "Dora",
+            "Steve"
+        };
+        lista.Add("Gaby");
+        //foreach (var nombre in lista)
+        //  {
+        //      Console.WriteLine(nombre);
+        //  }
+        for (int i = 0; i < lista.Count; i++)
+        {
+            Console.WriteLine(lista[i]);
+        }
+
+
+
+
+        #endregion
+        Console.ReadKey();
+
+
+
+    }
+}
